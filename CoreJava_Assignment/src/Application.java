817771e3565c500ce4618d26d633a47fb6620562
VaishnaviFale <vaishnavi.fale28@gmail.com>
2021-08-04 20:40:06 +0530

    Updating the program

--- CoreJava_Assignment/src/Application.java
+++ CoreJava_Assignment/src/Application.java
@@ -2,36 +2,33 @@ import java.util.Scanner;
 import java.util.*;
 public class Application {
 	public static void main(String args[])
-	{
+	{    
+	    int i=0,flag=0;
+		while(i<3) {
+			
 		Scanner scan= new Scanner(System.in); 
 		System.out.println("Please Enter your Login Name :");
 		String user= scan.nextLine();   
 		System.out.println("Please Enter your Password :");
 		String pwd= scan.nextLine(); 
-		
 		String user1="Vaishnavi";
-		String pwd1="root";
-		int flag=0;
-		
+		String pwd1="root";	
 		if(user1.equals(user) && pwd1.equals(pwd))
 		{
 			System.out.println("Welcome.."+user1);
+			flag=1;
+			break;
 		}
-		else if(true)
-		{   System.out.println();
-			System.out.println("Login name or Password is Incorrect");
-			System.out.println("Try Again !");System.out.println();
-			System.out.println("Please Enter your Login Name :");
-			String user2= scan.nextLine();   
-			System.out.println("Please Enter your Password :");
-			String pwd2= scan.nextLine(); 
-			if(user1.equals(user2) && pwd1.equals(pwd2))
-			{
-				System.out.println("Welcome.."+user1); flag=1;
-			}
-			if(flag==0) {System.out.println(); System.out.println("Contact Admin");}
+		System.out.println();
+		System.out.println("Incorrect Login Name or Password !");
+		System.out.println("***Try Again***");
+		System.out.println();
+		i++;
 		}
-			scan.close();
+		if(flag==0)
+		{ System.out.println();
+		System.out.println("***Contact Admin***");}
+		
 	}
 
 }
